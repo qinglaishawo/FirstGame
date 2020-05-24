8974e861201a5fec45ec4fbceaa7d4e1c800e8d0
胡晟 <hs>
2020-05-25 01:26:17 +0800

    准备添加敌人

---
+++ FirstGame/Assets/FinalMovement.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FinalMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    private Animator anim;
+
+    public float speed, jumpForce;
+    public Transform groundCheck;
+    public LayerMask ground;
+
+    public bool isGround, isJump;
+
+    bool jumpPressed;
+    int jumpCount;
+
+    public Text cherryText;
+    public Text gemText;
+    public int cherryCount;
+    public int gemCount;
+    private string preCollectionName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetButtonDown("Jump")&&jumpCount>0)
+        {
+            jumpPressed = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position,0.15f,ground);
+        GroundMovement();
+        Jump();
+        SwitchAnim();
+    }
+
+    void GroundMovement()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalMove*speed,rb.velocity.y);
+
+        if (horizontalMove != 0)
+        {
+            transform.localScale = new Vector3(horizontalMove,1,1);
+        }
+        anim.SetFloat("running", Mathf.Abs(horizontalMove));
+    }
+
+    void Jump()
+    {
+        if (isGround)
+        {
+            jumpCount = 2;
+            isJump = false;
+        }
+        if (jumpPressed&&isGround)
+        {
+            isJump = true;
+            rb.velocity = new Vector2(rb.velocity.x,jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+        else if (jumpPressed && jumpCount > 0 && isJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x,jumpForce);
+            jumpCount--;
+            jumpPressed = false;                                                                                                                    
+        }
+        if (!isGround)
+        {
+            jumpPressed = false;
+        }
+    }
+
+    void SwitchAnim()
+    {
+        if (isJump)
+        {
+            anim.SetBool("jumping", true);
+            anim.SetBool("falling", false);
+        }
+        if (anim.GetBool("jumping"))
+        {
+            if (rb.velocity.y < 0)
+            {
+                anim.SetBool("jumping",false);
+                anim.SetBool("falling",true);
+            }
+        }
+        if (anim.GetBool("falling"))
+        {
+            if (isGround)
+            {
+                anim.SetBool("idle", true);
+                anim.SetBool("falling", false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collection")
+        {
+            if (preCollectionName != collision.name)
+            {
+                if (collision.name.Contains("Cherry"))
+                {
+                    cherryCount++;
+                    cherryText.text = cherryCount.ToString();
+                }
+                if (collision.name.Contains("gem"))
+                {
+                    gemCount++;
+                    gemText.text = gemCount.ToString();
+                }
+                preCollectionName = collision.name;
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
